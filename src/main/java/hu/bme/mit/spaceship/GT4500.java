4c79b2494255258c5b8666ca54e7ad6f7e4d1273
sydnikat <tonycalifor1@gmail.com>
2019-05-10 08:56:19 +0200

    Initialize tests

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -15,6 +15,11 @@ public class GT4500 implements SpaceShip {
     this.secondaryTorpedoStore = new TorpedoStore(10);
   }
 
+  public GT4500(TorpedoStore prTorpedoStore, TorpedoStore scTorpedoStore){
+    this.primaryTorpedoStore = prTorpedoStore;
+    this.secondaryTorpedoStore = scTorpedoStore;
+  }
+
   public boolean fireLaser(FiringMode firingMode) {
     // TODO not implemented yet
     return false;
--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,27 +9,35 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore mockPr;
+  private TorpedoStore mockSc;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockPr = mock(TorpedoStore.class);
+    mockSc = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockPr, mockSc);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    //when(mockPr.getTorpedoCount()).thenReturn(10);
+    when(mockPr.fire(1)).thenReturn(true);
+    when(mockPr.isEmpty()).thenReturn(false);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
+    verify(mockPr, times(1)).fire(1);
+    verify(mockPr, times(1)).isEmpty();
     assertEquals(true, result);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    //when(mockPr.getTorpedoCount()).thenReturn(10);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
